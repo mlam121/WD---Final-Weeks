7256ddfa986e972d6712a006c2b7569877fd2cce
mylena <mylenalam101@gmail.com>
2026-05-05 09:54:48 -0500

    finish

---
+++ functional/word-counter.js
@@ -0,0 +1,31 @@
+// 1. Find the first paragraph 
+// 2. Count the number of words in it 
+// 3. Calculate how long it would take to read 
+//    Avg reading speed in US: ~250 words per minute 
+// 4. update the paragraph to include the reading time 
+
+
+function insertReadingTime() {
+  let $p = document.querySelector("p");
+  let words = countWords($p.innerText);
+  let duration = wordsToMinutes(words);
+  $p.innerText += " (Reading time: " + duration + " minutes)";
+}
+
+insertReadingTime(document.querySelector("p"));
+
+// Word Counter Function
+function countWords(the_text) {
+  return the_text.split(" ").length;
+}
+
+function wordsToMinutes(words) {
+  return words / 250;
+}
+
+let $paragraphs = document.querySelectorAll("p");
+let count = $paragraphs.length;
+
+for (let i = 0; i < count; i++) {
+  insertReadingTime($paragraphs[i]);
+}
